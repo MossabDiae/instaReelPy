2037f1dc87f17829eb8be99cfac7cada5115cde8
mossabdiae <mossabdiae@gmail.com>
2023-01-09 02:14:47 +0100

    add: sketched main logic

---
+++ instareelpy/mkreel.py
@@ -0,0 +1,28 @@
+import moviepy.editor as mpy
+import argparse
+
+
+# Setting vars
+container_size = (1080, 1080)
+DEBUG = True
+
+# Collect data from user (video file, image, times)
+# TODO: Set argparse
+
+# video, times
+# image
+
+
+# Generate a video-clip from sub-clips array
+# merge_subclips(video, [(t1, t2), (), ...]) -> vclip made from sclips array + animation
+
+# Put things together
+# 1- Calculate sizes + set positions based on container vars
+
+# 2- Set image + container duration based on vclip 
+
+# 3- Put things together
+
+# Debug
+
+# Export
